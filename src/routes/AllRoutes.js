a6ca2f5c8d05a6818a1a7f840ccff3155ae35948
SudhanshuSingh884 <sudhanshus.it.21@nitj.ac.in>
2024-07-05 17:17:06 +0530

    new changes

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -7,6 +7,7 @@ export const AllRoutes = () => {
     <Routes>
       
         <Route path="/" element={<HomePage/>}/>
+        <Route path="/" element={<HomePage/>}/>
         <Route path="/products" element={<ProductsList/>}/>
     </Routes>
    </>
